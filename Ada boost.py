58689ad33e963b305bca3aa2a13ae0970118436e
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-04-21 21:36:50 -0500

    Create Ada boost.py

---
+++ Ada boost.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+class AdaBoost:
+    def __init__(self, base_estimator, n_estimators=50, learning_rate=1):
+        self.base_estimator = base_estimator
+        self.n_estimators = n_estimators
+        self.learning_rate = learning_rate
+        self.estimators = []
+        self.weights = []
+        
+    def fit(self, X, y):
+        # Initialize sample weights
+        sample_weight = np.ones(len(X)) / len(X)
+        
+        for i in range(self.n_estimators):
+            # Train a weak learner on the training set using the current sample weights
+            estimator = self.base_estimator()
+            estimator.fit(X, y)
+            
+            # Compute the error of the weak learner on the training set
+            y_pred = estimator.predict(X)
+            error = np.sum(sample_weight * (y != y_pred)) / np.sum(sample_weight)
+            
+            # Compute the weight of the weak learner in the final classifier
+            weight = self.learning_rate * np.log((1 - error) / error)
+            self.weights.append(weight)
+            self.estimators.append(estimator)
+            
+            # Update the sample weights based on the performance of the weak learner
+            sample_weight *= np.exp(weight * (y != y_pred))
+            sample_weight /= np.sum(sample_weight)
+    
+    def predict(self, X):
+        # Combine the weak learners into a final strong classifier
+        y_pred = np.zeros(len(X))
+        for i in range(len(self.estimators)):
+            y_pred += self.weights[i] * self.estimators[i].predict(X)
+        
+        # Make predictions on the testing set using the final classifier
+        return np.sign(y_pred)
