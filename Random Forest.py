0c248af7666e67b3a411f595f2b9a0b38730cce8
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2023-05-05 22:58:36 -0500

    Update Random Forest.py

--- Random Forest.py
+++ Random Forest.py
@@ -1,36 +1,60 @@
+# Random forest class according to the requriemnts.
 class RandomForest:
-    def __init__(self, max_depth,criterion,num_trees=100,min_features =4):
-        #self.classifier = classifier
+    # Initializing the class attributes.
+    """
+    max_depth: The maximum depth of each decision tree in the random forest.
+    criterion: The criterion used for evaluating the quality of splits in each decision tree.
+    num_trees: The number of decision trees in the random forest.
+    min_features: The minimum number of features to consider when making splits in each decision tree.
+    forest: A list of DecisionTree objects.
+    """
+    def __init__(self, max_depth, criterion, num_trees=100, min_features = 4):
         self.num_trees = num_trees
         self.min_features = min_features
-        self.max_depth=max_depth
-        self.criterion=criterion
+        self.max_depth = max_depth
+        self.criterion = criterion
         self.forest = []
 
-    def fit(self, X, y):
-        num_features = X.shape[1]
+    # Defining the fit function that calls the decision tree classifier.
+    def fit(self, X, y, weights = None):
+        # Finding the number of features in each decision tree.
+        n_features = X.shape[1]
+        # Iterating through each of the decision trees.
         for i in range(self.num_trees):
-            # Sampling with replacement
-            indices = np.random.choice(X.shape[0], size=X.shape[0], replace=True)
-            X_sample = X[indices]
-            y_sample = y[indices]
-            # Selecting random subset of features
-            num_selected_features = np.random.randint(self.min_features, num_features + 1)
-            selected_features = np.random.choice(num_features, size=num_selected_features, replace=False)
-            X_sample = X_sample[:, selected_features]
-            # Train decision tree
-            clf = DecisionTreeClassifier(max_depth=self.max_depth,criterion=self.criterion)
-            clf.fit(X_sample, y_sample)
-            self.forest.append((clf, selected_features))
+            # Bootstrapping
+            # 1) Sampling the data with replacement.
+            indices = np.random.choice(X.shape[0], size = X.shape[0], replace = True)
+            # Making subsets of the data in X_data and y_data.
+            X_data, y_data = X[indices], y[indices]
+            # 2) Selecting random subset of features
+            n_selected_features = np.random.randint(self.min_features, n_features + 1)
+            # Selecting the random subset of features and stores in selected_features.
+            selected_features = np.random.choice(n_features, size = n_selected_features, replace = False)
+            # taking only the selected subset features data.
+            X_data = X_data[:, selected_features]
+            # Training the Decision Tree 
+            Tree = DecisionTree(max_depth = self.max_depth, criterion = self.criterion)
+            # Fitting the tree
+            Tree.fit(X_data, y_data)
+            # Appending the trained decision tree and it's indices of the selected features to the forest list.
+            self.forest.append((Tree, selected_features))
 
+    # Function to predict the output of the random forest.
+    # Aggregation - Works by maximum voting.
     def predict(self, X):
-        votes = np.zeros((X.shape[0],))
-        for clf, selected_features in self.forest:
-            X_sample = X[:, selected_features]
-            pred = clf.predict(X_sample)
-            votes[pred == 1] += 1
-        return (votes >= (len(self.forest) / 2)).astype(int)
+        # Creating an array to keep track of the votes
+        votes_array = np.zeros((X.shape[0],))
+        # Iterating through each decision trees and finding the prediction.
+        for Tree, selected_features in self.forest:
+            prediction = Tree.predict(X[:, selected_features])
+            # Updtating the values of the predictions of each decision tree to the votes_array.
+            votes_array[prediction == 1] += 1
+        # Returing the majority of votes as prediction.
+        return (votes_array >= (len(self.forest) / 2)).astype(int)
     
-    def Accuray(self, X_test, y_test):
-       y_pred = self.predict(X_test)
-       return np.mean(y_pred == y_test)
+    # Function to find the Accuracy.
+    def Accuracy(self, X_test, y_test):
+        # Finding the y_pred inorder to compare with the y_test.
+        y_pred = self.predict(X_test)
+        # Returing the accuracy of the prediction.
+        return round(np.mean(y_pred == y_test)*100, 3)
